056eedcde00d05382a5fd3217e9e67c8195df44f
nertsal <nestralnertsal@gmail.com>
2026-03-07 12:35:34 +0300

    cutting plants

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -7,10 +7,27 @@ use crate::{model::*, prelude::*, render::*, ui::context::UiContext};
 pub struct GameState {
     context: Context,
     ui_context: UiContext,
+    framebuffer_size: vec2<usize>,
 
     render: GameRender,
     model: Model,
     ui: GameUI,
+
+    cursor_pos: vec2<f64>,
+    cursor_world_pos: vec2<FCoord>,
+    cursor_grid_pos: vec2<ICoord>,
+    input_state: InputState,
+}
+
+#[derive(Debug)]
+pub enum InputState {
+    Idle,
+    UseItem(Item),
+}
+
+#[derive(Debug, Clone)]
+pub enum Item {
+    Scissors,
 }
 
 impl GameState {
@@ -20,8 +37,30 @@ impl GameState {
             model: Model::new(),
             ui: GameUI::new(),
 
+            cursor_pos: vec2::ZERO,
+            cursor_world_pos: vec2::ZERO,
+            cursor_grid_pos: vec2::ZERO,
+            input_state: InputState::Idle,
+
             ui_context: UiContext::new(context.clone()),
+            framebuffer_size: vec2(1, 1),
             context,
+        }
+    }
+
+    fn left_click(&mut self) {
+        if let InputState::UseItem(item) = &self.input_state {
+            let target = self.cursor_grid_pos;
+            match item {
+                Item::Scissors => {
+                    if let Some(plant) = self.model.grid.plants.iter_mut().find(|plant| {
+                        plant.stem.contains(&target) || plant.leaves.contains(&target)
+                    }) {
+                        plant.stem.clear();
+                        plant.leaves.clear();
+                    }
+                }
+            }
         }
     }
 }
@@ -32,6 +71,15 @@ impl geng::State for GameState {
 
         let delta_time = Time::new(delta_time as f32);
         self.model.update(delta_time);
+
+        match &mut self.input_state {
+            InputState::Idle => {
+                if self.ui.scissors.mouse_left.clicked {
+                    self.input_state = InputState::UseItem(Item::Scissors);
+                }
+            }
+            InputState::UseItem(_) => {}
+        }
     }
 
     fn handle_event(&mut self, event: geng::Event) {
@@ -41,12 +89,25 @@ impl geng::State for GameState {
             }
             geng::Event::CursorMove { position } => {
                 self.ui_context.cursor.cursor_move(position.as_f32());
+                self.cursor_pos = position;
+                self.cursor_world_pos = self
+                    .model
+                    .camera
+                    .screen_to_world(self.framebuffer_size.as_f32(), position.as_f32())
+                    .as_r32();
+                self.cursor_grid_pos = self.model.grid_visual.world_to_grid(self.cursor_world_pos);
+            }
+            geng::Event::MousePress {
+                button: geng::MouseButton::Left,
+            } => {
+                self.left_click();
             }
             _ => {}
         }
     }
 
     fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
+        self.framebuffer_size = framebuffer.size();
         ugli::clear(
             framebuffer,
             Some(self.context.assets.palette.background),
